7c9263225171e90e22907805ac7239d242427a82
cch <1136856486@qq.com>
2018-08-21 01:48:06 +0800

    hello

--- src/main/java/com/cch/spring_tx/App.java
+++ src/main/java/com/cch/spring_tx/App.java
@@ -1,13 +1,21 @@
 package com.cch.spring_tx;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Hello World!" );
+        StringUtils.isNotBlank("123");
+        FileUtils.writeStringToFile(new File("C:/hello.txt"), "data", "utf-8");
     }
 }
